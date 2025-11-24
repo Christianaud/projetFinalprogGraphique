794fee52f665e024c0cbae32b4ceb386241bd230
zeebegener-commits <zeebegener@gmail.com>
2025-11-24 13:43:42 -0500

    Clients + Projets classes

---
+++ projet/projet/ClassesParJacques/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet.ClassesParJacques
+{
+    class Client
+    {
+        int id;
+        string nom;
+        string adresse;
+        int num_tel;
+        string email;
+
+        public Client(int id, string nom, string adresse, int num_tel, string email)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.adresse = adresse;
+            this.num_tel = num_tel;
+            this.email = email;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public string Email { get => email; set => email = value; }
+        public int Num_tel { get => num_tel; set => num_tel = value; }
+
+        public override string ToString()
+        {
+            return $"Id: {id}, Nom: {nom}, Num_tel: {Num_tel} , Adresse: {adresse}, Email: {email}";
+        }
+    }
+}
---
+++ projet/projet/ClassesParJacques/Projet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet.ClassesParJacques
+{
+    internal class Projet
+    {
+        int numero;
+        string titre;
+        DateTime dateDebut;
+        string description;
+        int budget;
+        int nbrEmployes;
+        int totalSalaire;
+        int idClient;
+        string statut;
+
+        public Projet(int numero, string titre, DateTime dateDebut, string description, int budget, int nbrEmployes, int totalSalaire, int idClient, string statut)
+        {
+            this.numero = numero;
+            this.titre = titre;
+            this.dateDebut = dateDebut;
+            this.description = description;
+            this.budget = budget;
+            this.nbrEmployes = nbrEmployes;
+            this.totalSalaire = totalSalaire;
+            this.idClient = idClient;
+            this.statut = statut;
+        }
+
+        public int Numero { get => numero; set => numero = value;}
+        public string Titre { get => titre; set => titre = value;}
+        public DateTime DateDebut { get => dateDebut; set => dateDebut = value;}
+        public string Description { get => description; set => description = value;}
+        public int Budget { get => budget; set => budget = value;}
+        public int NbrEmployes { get => nbrEmployes; set => nbrEmployes = value; }
+        public int TotalSalaire { get => totalSalaire; set => totalSalaire = value; }
+        public int IdClient { get => idClient; set => idClient = value; }
+        public string Statut { get => statut; set => statut = value; }
+
+        public override string ToString()
+        {
+            return $"Numero: {numero}, Titre: {titre}, DateDebut: {dateDebut.ToString()}, Description: {description}, Budget: {budget}, Nbr_employes: {nbrEmployes}, TotalSalaire: {totalSalaire}, IdClient: {idClient}, Statut: {statut}";
+        }
+    }
+}
