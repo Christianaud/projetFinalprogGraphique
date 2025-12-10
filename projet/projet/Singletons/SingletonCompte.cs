eca87604e49479a304ef29f2b9570ef259a81b1f
Christianaud <nirylanjachristianaud@gmail.com>
2025-12-10 14:44:45 -0500

    commit

--- projet/projet/Singletons/SingletonCompte.cs
+++ projet/projet/Singletons/SingletonCompte.cs
@@ -1,12 +1,16 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Microsoft.WindowsAppSDK.Runtime.Packages;
+using MySql;
+using MySql.Data.MySqlClient;
+using projet.classes;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace projet.Singletons
+namespace projet.Classes
 {
     internal class SingletonCompte
     {
@@ -15,6 +19,7 @@ namespace projet.Singletons
         string password;
         bool doesAdminExist;
         bool isConnected = false;
+        List<Admin> listeAdmins;
 
         // vv Doit changer vv
 
@@ -31,6 +36,45 @@ namespace projet.Singletons
                 instance = new SingletonCompte();
             return instance;
         }
+
+        public void getAllAdmins()
+        {
+            listeAdmins.Clear();
+
+            try
+            {
+                using MySqlConnection con = new MySqlConnection(connectionString);
+                using MySqlCommand commande = con.CreateCommand();
+                commande.CommandText = "Select * from administrateur";
+                con.Open();
+                using MySqlDataReader r = commande.ExecuteReader();
+                while (r.Read())
+                {
+                    string nomUsager = r.GetString("nomUsager");
+                    string motDePasse = r.GetString("motDePasse");
+                    Admin admin = new Admin(nomUsager, motDePasse);
+                    listeAdmins.Add(admin);
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public bool doesInfoMatch(string nomUtilisateur, string motDePasse)
+        {
+            Admin check = new Admin(nomUtilisateur, motDePasse);
+            if (listeAdmins.Contains(check))
+            {
+                return true;
+                isConnected = true;
+            }
+            else
+                return false;
+        }
+
+        public bool IsConnected { get => isConnected; set => isConnected = value; }
 
         // Vérifie l'esxistence du compte admin. Lancer au démarrage de l'app.
         public void VerifyAdmin()
@@ -66,7 +110,7 @@ namespace projet.Singletons
             {
                 using MySqlCommand commande = new MySqlCommand();
                 commande.Connection = con;
-                commande.CommandText = "insert into admin(username, password) values(@username, @password) ";
+                commande.CommandText = "insert into admin(username, password) values(@username, @password)";
                 commande.Parameters.AddWithValue("@username", username);
                 commande.Parameters.AddWithValue("@password", password);
                 con.Open();
--- projet/projet/classes/Admin.cs
+++ projet/projet/classes/Admin.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace projet.ClassesParJacques
+namespace projet.classes
 {
     internal class Admin
     {
